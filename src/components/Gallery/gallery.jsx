8c812b045c5266a71f85616402c91c6f5f0cde65
Kata Sworek <katarzyna.sworek@gmail.com>
2019-02-05 13:46:00 +0100

    zmiany w gallery -  nextButton skr kodu

--- src/components/Gallery/gallery.jsx
+++ src/components/Gallery/gallery.jsx
@@ -1,122 +1,77 @@
 import React from "react";
 
 class Gallery extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gallery: null,
-      page: 1 ,
-     };
-  }
- //button next - metoda onClick pobranie danych - kolejne strony O TYM SAMYM TAGU 
-    onClick = () => {
-      this.setState({
-        page: this.state.page +1 
-      },() =>{
-        fetch(`https://api.pexels.com/v1/search?query=${this.props.term}`,{
-          headers: {
-            "Authorization": "563492ad6f91700001000001a3f69df7408e4f58b2536d0fd82edb30"
-          }
-        }).then(response => {
-          if (response.ok) {
-            return response.json();
+    constructor(props) {
+        super(props);
+        this.state = {
+            gallery: null
+        };
+    }
 
-          }else{
-            console.log(response)
-          }
+    componentDidMount() {
+        this.handleRequest()//Wywołaj metodę pobierającą zdjęcia przy starcie aplikacji (bez przekazywania parametrów)
+    }
+    componentDidUpdate(prevProps) {
+        const {term} = this.props;
+        if(prevProps.term !== term) { //Sprawdzenie czy tekst(tag) się zmienił
+            this.handleRequest(term) //Wywołaj metodę pobierającą zdjęcia z przekazanym tekstem(tagiem) jako pierwszy parametr metody handleRequest. Nie przekazuje drugiego parametru.
         }
-      ).then(data => {
-        console.log(data);
-        this.setState({
-          gallery:data.photos
+    }
+    handleRequest = (query,prevOrNext) => { //Metoda pobierająca zdjęcia - jako parametry może (nie musi) przyjmować tekst oraz url do poprzedniej/następnej strony
+        const {gallery} = this.state; //Destrukturyzacja
+        let requestUrl = ''; //Deklaracja zmiennej przechowującej adres Url API (lub jego końcówkę)
+        if(prevOrNext) { //Jeżeli został przekazany argument prevOrNext
+            requestUrl = prevOrNext; //Przypisz zmiennej ten argument (będący adresem Url API)
+        } else if(query) { //Jeżeli zostało przekazane jakieś query(tag)
+            requestUrl = `https://api.pexels.com/v1/search?query=${query}`; //Przypisz zmiennej adres z tym query
+        } else if(!gallery || !query) { //Jeżeli state gallery jest puste(componentDidMount), albo nie ma query(zostało skasowane)
+            requestUrl = 'https://api.pexels.com/v1/curated'; //Przypisz zmiennej adres domyślny
+        }
+        fetch(requestUrl, { //Ściągaj z API po adresie requestUrl
+            headers: {
+                "Authorization": "563492ad6f9170000100000119b2c277c7c340bbab928823d2551d3b",
+                "accept": "application/json"
+            }
         })
-      })  
-    })
-  }
-       
-
- getNextPage =()=>{
-      fetch(this.state.nextPageUrl)   
-        .then( resp =>resp.json())
-        .then(data=> {
+        .then(resp => {
+            if(resp.ok) {
+                return resp.json()
+            } else {
+                console.log(resp)
+            }
+        })
+        .then(data => {
+            console.log(data);
             this.setState({
-            nextPageUrl: data.nextPageUrl
-       })
-      })
-    }
-                   
+                gallery: data //Cały objekt galerii
+            })
+        })
+    };
 
-//POBRANIE zdjęć z api 
-
-  componentDidMount() {
-    fetch(`https://api.pexels.com/v1/curated`,{
-      headers: {
-        "Authorization": "563492ad6f91700001000001a3f69df7408e4f58b2536d0fd82edb30"
-      }
-    })
-     .then( resp => resp.json())
-     .then(data => {
-      console.log(data)
-      this.setState({
-        gallery:data.photos // objekt -photo-galeria 
-      }) 
-    })
-    
-       
-  }
-  
-        
-//pobieranie zdjęć z API po tagach
-
-  componentDidUpdate(prevProps){
-    if (prevProps.term != this.props.term) {//spr czy term props uległ zmianie 
-      fetch(`https://api.pexels.com/v1/search?query=${this.props.term}`,{
-        headers: {
-          "Authorization": "563492ad6f91700001000001a3f69df7408e4f58b2536d0fd82edb30"
+    render() {
+        const {gallery} = this.state;
+        if (gallery === null) {
+            return null //Nie renderuj niczego, jeżeli dane nie zostały jeszcze ściągnięte
         }
-      })
-       .then( resp => resp.json())
-       .then(data => {
-        console.log(data)
-        this.setState({
-          gallery:data.photos // objekt -photo-galeria 
-        }) 
-      })
-
+        return (
+            <div>
+                <ul>
+                    {gallery.photos.map(photo => { //Mapuj po photos z obiektu galerii
+                        return (
+                            <li className="galleryBody" key={photo.id}>
+                                <div className="container">
+                                    {/* <div>{photo.photographer} </div> */}
+                                    <img className="pictures" src={photo.src.medium} alt=""/>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+                {/*Button Previous i Next - pobranie poprzednich/kolejnych stron dla albumu o danym tagu, lub stron dla albumu strony domyślnej*/}
+                {gallery.prev_page && <button className="prevButton" onClick={() => this.handleRequest(null, gallery.prev_page)}>Previous</button>} {/*Jeżeli obiekt gallery(ze state) posiada klucz prev_page(czyli nie jest pierwszą stroną) to wyrenderuj przycisk Previous. Przycisk na kliknięcie wywołuje metodę handleRequest przekazując jej parametr null jako query oraz atrybut gallery.prev_page(będący adresem Url do następnej strony) jako prevOrNext. Obecność atrybutu prev_page wynika z budowy obiektu(dokumentacja API)).*/}
+                {gallery.next_page && <button className="nextButton" onClick={() => this.handleRequest(null, gallery.next_page)}>Next</button>} {/*Jak wyżej, z tym że atrybut next_page oczywiście nie pojawia się tylko dla ostatniej strony(a nie pierwszej jak dla prev_page)*/}
+            </div>
+        )
     }
-
-  }
-
- 
-
- render() {
-  if (this.state.gallery === null) {
-    return null
-  }
-  return <div>
-    <ul>
-        {this.state.gallery.map (photo => {
-            return <li className="galleryBody" key={photo.id}>
-            <div className="container">          
-                {/* <div>{photo.photographer} </div> */}
-                <img className="pictures"                       
-                      src={photo.src.medium} alt=""></img>        
-              </div>
-          </li> 
-          })}
-        
-      </ul>
-      <button className="nextButton"
-               onClick ={this.onClick}>Next</button>
-    </div>
-  }
-    
-  }
-  
- 
- 
- 
- 
-
-  
+}
 export default Gallery;
